5fc3901f203501b84c45f182fa885f6a9e3d5221
Oceane Feltrin <oceanefeltrin@yahoo.fr>
2023-02-11 17:14:17 +0100

    project 10 grocery bud completed

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import { useEffect } from "react"
 
-const Alert = () => {
+const Alert = ({ type, msg, removeAlert, list}) => {
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      removeAlert()
+    }, 3000);
+    return () => clearTimeout(timeOut)
+  }, [list])
   return (
-    <div>Alert</div>
+    <p
+        className={`alert alert-${type}`}
+    >
+      {msg}
+    </p>
   )
 }
 
--- src/components/List.js
+++ src/components/List.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import { FaEdit, FaTrash } from "react-icons/fa"
 
-const List = () => {
+const List = ({ items, removeItem, editItem }) => {
   return (
-    <div>List</div>
+    <div className='gorcery-list'>
+      {items.map((item) => {
+        const { id, title } = item
+        return (
+          <article className='grocery-item' key={id}>
+            <p className='title'>
+              {title}
+            </p>
+            <div className='btn-container'>
+              <button
+                  type='button'
+                  className='edit-btn'
+                  onClick={() => editItem()}
+              >
+                <FaEdit/>
+              </button>
+              <button
+                  type='button'
+                  className='delete-btn'
+                  onClick={() => removeItem(id)}
+              >
+                <FaTrash/>
+              </button>
+            </div>
+          </article>
+        )
+      })}
+    </div>
   )
 }
 
